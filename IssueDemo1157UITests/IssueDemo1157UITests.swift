bb4d8420434e6764239ff6088433a1e8277deb5e
Brandon Fraune <brandon.fraune@gmail.com>
2024-09-30 15:46:54 -0500

    demonstrate issue

--- IssueDemo1157UITests/IssueDemo1157UITests.swift
+++ IssueDemo1157UITests/IssueDemo1157UITests.swift
@@ -6,38 +6,17 @@
 //
 
 import XCTest
+import Nimble
 
 final class IssueDemo1157UITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testTextVisible() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        sleep(1) // Hack to wait for UI to paint
+        
+        let textToCheck = app.staticTexts["Hello, world!"]
+        print("### textToCheck exists=\(textToCheck.exists)")
+        
+        expect(app.staticTexts["Hello, World!"].exists).to(beTrue())
     }
 }
