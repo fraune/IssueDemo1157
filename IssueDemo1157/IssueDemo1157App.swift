b79ff7a9280fdb58d28007917fd68d7b17424d23
Brandon Fraune <brandon.fraune@gmail.com>
2024-09-30 15:37:27 -0500

    project init

---
+++ IssueDemo1157/IssueDemo1157App.swift
@@ -0,0 +1,17 @@
+//
+//  IssueDemo1157App.swift
+//  IssueDemo1157
+//
+//  Created by Brandon Fraune on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct IssueDemo1157App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
